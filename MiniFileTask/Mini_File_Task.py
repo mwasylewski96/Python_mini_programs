5c10f72103065db2be366ced16fecddb24b3b90b
Marcin Wasylewski <m.wasylewski96@gmail.com>
2022-10-21 00:46:27 +0200

    Mini File Task

---
+++ MiniFileTask/Mini_File_Task.py
@@ -0,0 +1,17 @@
+with open("names_and_surnames.txt", "r", encoding="UTF-8") as file:
+    readfile = file.readlines()
+SeparatedElements = []
+for line in readfile:
+    temp = line.strip()
+    SeparatedElements.append(temp.split(" "))
+print(SeparatedElements)
+SeparatedNames = [i[0] for i in SeparatedElements]
+SeparatedSurnames = [i[1] for i in SeparatedElements]
+with open("Names.txt", "w", encoding="UTF-8") as file:
+    for name in SeparatedNames:
+        file.write(name)
+        file.write("\n")
+with open("Surnames.txt", "w", encoding="UTF-8") as file:
+    for surname in SeparatedSurnames:
+        file.write(surname)
+        file.write("\n")
