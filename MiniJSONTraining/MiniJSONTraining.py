ac5a9b6f5d8abe0e35e1667dd526cb7f7360fad5
Marcin Wasylewski <m.wasylewski96@gmail.com>
2022-10-22 11:06:46 +0200

    JSON training

---
+++ MiniJSONTraining/MiniJSONTraining.py
@@ -0,0 +1,43 @@
+"""
+
+JSON
+
+"""
+import json
+
+film = {
+    "title" : "Ale ja nie będę tego robił!",
+    "release_year" : 1969,
+    "won_oscar" : True,
+    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
+    "budget" : None,
+    "credits" : {
+            "director" : "Arkadiusz Włodarczyk",
+            "writer" : "Alan Burger",
+            "animator" : "Anime Animatrix"
+            }
+}
+filmwJSON = json.dumps(film, ensure_ascii=False)
+print(filmwJSON)
+with open("FilmWJSONType.json", "w", encoding="UTF-8") as file:
+    file.write(filmwJSON)
+
+
+"""
+{  
+   "title":"Ale ja nie będę tego robił!",
+   "release_year":1969,
+   "won_oscar":true,
+   "actors":[  
+      "Arkadiusz Włodarczyk",
+      "Wiolleta Włodarczyk"
+   ],
+   "budget":null,
+   "credits":{  
+      "director":"Arkadiusz Włodarczyk",
+      "writer":"Alan Burger",
+      "animator":"Anime Animatrix"
+   }
+}
+"""
+
