083f9a2d91b6952e7a8623461f697deb3750ed4e
Marcin Wasylewski <m.wasylewski96@gmail.com>
2022-10-22 13:35:10 +0200

    Jak pobrac zawartosc strony

---
+++ MiniPipPyPiTraining/MiniPipPyPiTraining.py
@@ -0,0 +1,7 @@
+import requests
+strona = requests.get("https://blog.home.pl/2020/07/strona-www-mala-firma-co-powinna-zawierac/")
+# wynik 404 to error nie ma takiej strony
+# wynik 200 to wszystko ok, jest taka strona
+# requests.get tak naprawde pobiera zawartosc strony
+with open("Zawartosc pobranej strony.txt", "w", encoding="UTF-8") as plik:
+    plik.write(strona.text)
