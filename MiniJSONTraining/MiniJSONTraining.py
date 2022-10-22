7abe0ad97c19e7bd65d9d4334680198ea5b7a774
Marcin Wasylewski <m.wasylewski96@gmail.com>
2022-10-22 11:46:07 +0200

    Polska Na Mundialach

--- MiniJSONTraining/MiniJSONTraining.py
+++ MiniJSONTraining/MiniJSONTraining.py
@@ -1,43 +1,28 @@
-"""
-
-JSON
-
-"""
 import json
 
-film = {
-    "title" : "Ale ja nie będę tego robił!",
-    "release_year" : 1969,
-    "won_oscar" : True,
-    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
-    "budget" : None,
-    "credits" : {
-            "director" : "Arkadiusz Włodarczyk",
-            "writer" : "Alan Burger",
-            "animator" : "Anime Animatrix"
-            }
+"Mini program zapisujący film do formatu JSON a potem odczytujący plik JSON z powrotem do Pythona"
+
+meczePolskinaMundialach = {
+    "mundial_2006" : ("Polska - Ekwador 0-2","Polska - Niemcy 0-1", "Polska - Kostaryka 2-0"),
+    "mundial_2010" : None,
+    "mundial_2014" : None,
+    "mundial_2018" : ("Polska - Senegal 1-2","Polska - Kolumbia 0-3", "Polska - Japonia 1-0")
 }
-filmwJSON = json.dumps(film, ensure_ascii=False)
-print(filmwJSON)
-with open("FilmWJSONType.json", "w", encoding="UTF-8") as file:
-    file.write(filmwJSON)
+for i in meczePolskinaMundialach:
+    print(i, meczePolskinaMundialach[i])
 
+print("")
 
-"""
-{  
-   "title":"Ale ja nie będę tego robił!",
-   "release_year":1969,
-   "won_oscar":true,
-   "actors":[  
-      "Arkadiusz Włodarczyk",
-      "Wiolleta Włodarczyk"
-   ],
-   "budget":null,
-   "credits":{  
-      "director":"Arkadiusz Włodarczyk",
-      "writer":"Alan Burger",
-      "animator":"Anime Animatrix"
-   }
-}
-"""
+zakodujwJSONmeczePolskinaMundialach = json.dumps(meczePolskinaMundialach, ensure_ascii=False)
 
+with open("Polska Na mundialach w XX wieku.json", "w", encoding="UTF-8") as plik:
+    plik.write(zakodujwJSONmeczePolskinaMundialach)
+
+with open("Polska Na mundialach w XX wieku.json", encoding="UTF-8") as plik:
+    odczytywanieJSONMeczePolskinaMundialach = json.load(plik)
+
+odczytywanieJSONMeczePolskinaMundialach["mundial_2006"] = tuple(odczytywanieJSONMeczePolskinaMundialach["mundial_2006"])
+odczytywanieJSONMeczePolskinaMundialach["mundial_2018"] = tuple(odczytywanieJSONMeczePolskinaMundialach["mundial_2006"])
+
+for line in odczytywanieJSONMeczePolskinaMundialach:
+    print(line, odczytywanieJSONMeczePolskinaMundialach[line])
