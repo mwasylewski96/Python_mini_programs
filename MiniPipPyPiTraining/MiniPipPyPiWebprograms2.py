6c5a1c9b5360a1645b47d4a88343ddeea732bcb8
Marcin Wasylewski <m.wasylewski96@gmail.com>
2022-10-25 16:03:13 +0200

    MiniPipPyPiwebprograms2.py

--- MiniPipPyPiTraining/MiniPipPyPiWebprograms2.py
+++ MiniPipPyPiTraining/MiniPipPyPiWebprograms2.py
@@ -3,10 +3,28 @@ import json
 r = requests.get("https://jsonplaceholder.typicode.com/todos")
 #r = requests.get("https://www.sport.pl/sport-hp/0,0.html")
 #tasks = json.loads(r.text)
+def count_task_frequency(tasks):
+    completedtasksFrequencyByUser = dict()
+    for entry in tasks:
+        if (entry["completed"] == True):
+            try:
+                completedtasksFrequencyByUser[entry["userId"]] += 1
+            except KeyError:
+                completedtasksFrequencyByUser[entry["userId"]] = 1
+    return completedtasksFrequencyByUser
+def users_with_top_completed_tasks(completedTaskFrequencyByUser):
+    usersIdWithMaxCompletedAmountOfTasks = []
+    maxAmountOfCompletedTasks = max(completedTaskFrequencyByUser.values())
+    for userId, numberOfCompletedTask in completedTaskFrequencyByUser.items():
+        if (numberOfCompletedTask == maxAmountOfCompletedTasks):
+            usersIdWithMaxCompletedAmountOfTasks.append(userId)
+    return usersIdWithMaxCompletedAmountOfTasks
+
 try:
     tasks = r.json()
 except json.decoder.JSONDecodeError:
     print("Niepoprawny format")
 else:
-    print("Wszystko ok")
-    print(tasks[0])
+    completedTaskFrequencyByUser = count_task_frequency(tasks)
+    usersWithTopCompletedTasks = users_with_top_completed_tasks(completedTaskFrequencyByUser)
+    print("Wręczamy ciasteczko mistrzunia dyscypliny do uzytkownikow o id:", usersWithTopCompletedTasks)
